6ed1cea9572b669e2eb73fe7bdebbe71d208f659
emilyjoyceline <c14210077@john.petra.ac.id>
2024-02-19 00:12:59 +0700

    updated .py & .ipynb file

--- job_scraping.py
+++ job_scraping.py
@@ -1,23 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+from datetime import datetime
+import urllib.parse 
+import os
 
-def get_url(position,location):
-    urls = []
-    for sequence in range(1, 11):
-        start_page = 1 + ((sequence - 1) // 10) * 10
-        template = 'https://www.timesjobs.com/candidate/job-search.html?from=submit&luceneResultSize=100&txtKeywords={}&postWeek=30&searchType=personalizedSearch&actualTxtKeywords={}&searchBy=0&rdoOperator=OR&txtLocation={}&pDate=I&sequence={}&startPage={}'
-        url = template.format(position,position, location, sequence, start_page)
-        urls.append(url)
-    return urls
 
 def scrape_jobs(position,location):
-    urls = get_url(position, location)
+    position = urllib.parse.quote_plus(position)
+    location = urllib.parse.quote_plus(location)
+    sequence = 1
     data = []
-    for url in urls:
+
+    while True:
+        start_page = 1 + ((sequence - 1) // 10) * 10
+        template = 'https://www.timesjobs.com/candidate/job-search.html?from=submit&luceneResultSize=100&txtKeywords=0DQT0{}0DQT0&postWeek=60&searchType=personalizedSearch&actualTxtKeywords={}&searchBy=0&rdoOperator=OR&txtLocation={}&pDate=I&sequence={}&startPage={}'
+        url = template.format(position,position, location, sequence, start_page)
+        
         html_text = requests.get(url).text
         soup = BeautifulSoup(html_text, 'lxml')
         jobs = soup.find_all('li',class_='clearfix job-bx wht-shd-bx')
+        print('jobs found', len(jobs),'url',url)
+
+        if not jobs:
+            break
+        sequence += 1
+
         for job in jobs:
             try:
                 job_role = job.find('h2').text.strip()
@@ -28,9 +36,9 @@ def scrape_jobs(position,location):
             except AttributeError:
                 company_name = ''
             try:
-                location = job.find('span').text.strip()
+                location_company = job.find('span').text.strip()
             except AttributeError:
-                location = ''
+                location_company = ''
             try:
                 posted_date = (job.find('span',class_='sim-posted').span.text).split('Posted')[-1].strip()
             except AttributeError:
@@ -48,33 +56,24 @@ def scrape_jobs(position,location):
             except AttributeError:
                 more_detail_link = ''
             
-            data.append({'Role':job_role, 'Company Name':company_name, 'Location':location, 'Posted Date':posted_date, 'Key Skill': skill,'Job Description':job_desc, 'More Detail':more_detail_link})
+            extract_date = datetime.today().strftime('%Y-%m-%d')
+            
+            data.append({'Role':job_role, 'Company Name':company_name, 'Location':location_company, 'Posted Date':posted_date,'Extracted Date':extract_date,'Key Skill': skill,'Job Description':job_desc, 'More Detail':more_detail_link})
+          
     df = pd.DataFrame(data)
-    df.to_csv('list_of_job_timesjobs.csv', index=False, encoding='utf-8')
+    
 
-# create main
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, f'list_of_{position}_timesjobs.csv')
+
+    df.to_csv(file_path, index=False, encoding='utf-8')
+
+
+
+
+
 if __name__ == '__main__':
-    # position = input('Enter the position you are looking for: ')
-    # location = input('Enter the location you are looking for: ')
-    position = 'software engineer'
-    location = 'bangalore'
-    scrape_jobs(position, location)
+    position = 'data analyst'
+    location = ''
 
-
-# html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=software+engineer&txtLocation=').text
-# soup = BeautifulSoup(html_text, 'lxml')
-# data = []
-
-# jobs = soup.find_all('li',class_='clearfix job-bx wht-shd-bx')
-# for job in jobs:
-#     job_role = job.find('h2').text.strip()
-#     company_name = job.find('h3',class_='joblist-comp-name').text.strip()
-#     location = job.find('span').text.strip()
-#     posted_date = (job.find('span',class_='sim-posted').span.text).split('Posted')[-1].strip()
-#     job_desc = job.find('label', string='Job Description:').find_next_sibling('a').previous_sibling.strip()
-#     data.append({'Role':job_role, 'Company Name':company_name, 'Location':location, 'Posted Date':posted_date, 'Job Description':job_desc})
-
-# df = pd.DataFrame(data)
-
-# df.to_csv('job_listing_timesjobs.csv', index=False, encoding='utf-8')
-
+scrape_jobs(position,location)
